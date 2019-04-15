ebd35b8018efb69d327745c7faac11c2181a1989
pco2699 <aenkun@gmail.com>
2019-04-16 00:32:17 +0900

    背景を追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,32 +24,19 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+body {
+  background-image: linear-gradient(blue, green);
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+@screen md {
+  body {
+    background-image: linear-gradient(red, green);
+  }
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+@screen lg {
+  body {
+    background-image: linear-gradient(yellow, green);
+  }
 }
 </style>
