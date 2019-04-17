ab58c55e4e290db7accaa059b5ebf9fe6d2e56ed
pco2699 <aenkun@gmail.com>
2019-04-18 01:06:08 +0900

    ロゴ追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,12 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-  background-color: #e4b8ff;
+  background-color: #0078cf;
+
+  background-position-x: center;
+  background-image: url('~assets/img/background/sp.png');
+  background-attachment: fixed;
+  background-size: 100% auto;
 }
 
 *,
@@ -25,23 +30,14 @@ html {
   margin: 0;
 }
 
-body {
-  background-color: #0078cf;
-  background-position-x: center;
-  background-image: url('~assets/img/background/sp.png');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: 100% auto;
-}
-
 @screen md {
-  body {
+  html {
     background-image: url('~assets/img/background/tablet.png');
   }
 }
 
 @screen lg {
-  body {
+  html {
     background-image: url('~assets/img/background/pc.png');
     background-size: 1440px auto;
   }
